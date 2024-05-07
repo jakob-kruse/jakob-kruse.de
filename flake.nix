6f53dbc524d7027703060bc0f027f199a8ee38c3
Jakob Kruse <contact@jakob-kruse.de>
2024-05-07 10:00:52 +0200

    Tue May  7 10:00:52 AM CEST 2024

--- flake.nix
+++ flake.nix
@@ -1,22 +1,53 @@
 {
-  description = "jakob-kruse.de";
-
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
-  };
-
-  outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
-      buildInputs = [
-        nodejs_21
-        nodePackages.pnpm
-      ];
-
-      shellHook = ''
-        export PATH=$PATH:./node_modules/.bin
-        alias ni="pnpm install"
-        alias nr="pnpm run"
-      '';
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    { self
+    , nixpkgs
+    , gitignore
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        packageJSON = pkgs.lib.importJSON ./package.json;
+        gitignoreSource = gitignore.lib.gitignoreSource;
+      in
+      {
+        packages = rec {
+          site-src = pkgs.mkYarnPackage rec {
+            name = "${packageJSON.name}-site-${version}";
+            version = packageJSON.version;
+            src = gitignoreSource ./.;
+            packageJson = "${src}/package.json";
+            yarnLock = "${src}/yarn.lock";
+            buildPhase = ''
+              yarn --offline build
+            '';
+            distPhase = "true";
+          };
+
+          default = pkgs.writeShellApplication {
+            name = packageJSON.name;
+            runtimeInputs = [ site-src pkgs.nodejs ];
+            text = ''
+              node ${site-src}/libexec/${packageJSON.name}/deps/${packageJSON.name}/build
+            '';
+          };
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.yarn pkgs.nodejs ];
+        };
+      }
+    );
 }
